1a8a67e00e2129202ee501035b83a4c4d105762e
queenjummai <ibrahimjummai23@gmail.com>
2022-09-12 20:40:19 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,10 @@ strand(time(0));
 n =rand() - RAND_MAX / 2;
 
 if (n > 0)
-	printf("%d is positive\n", n);
+	Printf("%d is positive\n", n);
 else if (n == 0)
-	printf("%d is zero\n",n);
+	Printf("%d is zero\n",n);
 else
-	printf("%d is negative\n",n);
+	Printf("%d is negative\n",n);
 return (0);
 }
