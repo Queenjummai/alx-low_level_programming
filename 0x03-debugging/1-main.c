f58269f57afdbc54a480d0be60336d59d25c1e2a
queenjummai <ibrahimjummai23@gmail.com>
2022-09-15 21:59:09 -0700

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - causes of infinite loop
+* Return: (0)
+*/
+
+int main(void)
+{
+printf('infinite loop incoming :(\n')
+i = 0;
+	 
+	 * i = 0;
+	 * while (i < 10)
+	 *{
+	 * putchar(i);
+	 *}
+	 *
+printf ('infinite loop avoided :\\0/n');
+return (0);
+}
