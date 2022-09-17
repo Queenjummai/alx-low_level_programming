f023c7ed40c53f7f5d07682166ed69152fa1dacb
queenjummai <ibrahimjummai23@gmail.com>
2022-09-17 12:28:51 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,31 @@
-int _putchar(char);
-void print_alphabet(void);
-void print_alphabet_10
+int _putchar(char c);
 
-v
-void print_alphabet_x10(void)
-;int_is lower(int c);
-int _is alpha(int c);
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
 int print_sign(int n);
-int print_last_digit (int);
-void jack _bever(void);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+void num(int r, int n);
