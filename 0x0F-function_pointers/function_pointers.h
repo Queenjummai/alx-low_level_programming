0dc65b11a7f2fa2f13f1f0f80db86469c6d406f2
queenjummai <ibrahimjummai23@gmail.com>
2022-10-12 22:23:12 -0700

    task F

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
-#ifndef _HEADER_
+#ifndef _FUNCTION_POINTERS_H
 
-#define _HEADER_
+#define _FUNCTION_POINTERS_H
+
+
 
 #include <stdlib.h>
 
@@ -11,5 +13,7 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 int int_index(int *array, int size, int (*cmp)(int));
+
+
 
 #endif
